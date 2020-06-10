94c0ba915e01f4f5e657bc0cdda5f466e85c997c
norrico31 <geraldbiasonjob31.com>
2020-06-10 13:41:27 +0800

    html boiler plate on php file

--- Hello-World.php
+++ Hello-World.php
@@ -1,3 +1,15 @@
 <?php 
     echo 'Hello World';
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my first php file</title>
+</head>
+<body>
+    <h1><?php echo 'hello, folks!'; ?></h1>
+</body>
+</html>
