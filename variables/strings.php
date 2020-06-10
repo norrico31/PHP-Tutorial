8900069bba4e71e388da914bf0621cc371e958d0
norrico31 <geraldbiasonjob31.com>
2020-06-10 20:15:03 +0800

    string data type

---
+++ variables/strings.php
@@ -0,0 +1,37 @@
+<?php 
+    $stringOne = 'geraldbiasonjob31@gmail.com';
+    $stringTwo = 'my email is ';
+
+    // STRING CONCATENATION
+    // echo $stringTwo . $stringOne;
+
+    $name = 'gerald';
+    
+    // IN DOUBLE QUOTES YOU CAN USE DIRECTLY THE VARIABLE
+    // echo "Hey my name is $name";
+
+    // TO USE DOUBLE QUOTES IN SENTENCES
+    // echo "the ninja screamed \"whaaaa\".";
+    // echo 'the ninjas screamed \"whaaa\".';
+
+    // echo $name[1];
+
+    // echo strlen($name);
+
+    // echo strtoupper($name); // to make the string uppercase
+    // echo strtolower($name); // to make the string lowercase
+
+    // to replace a string. gerald will replace to jerald
+    echo str_replace('g', 'j', $name);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strings</title>
+</head>
+<body>
+    
+</body>
+</html>
