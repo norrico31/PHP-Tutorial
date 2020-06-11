c34c1bec16f716ce4bb923f2acb14020d0275c43
norrico31 <geraldbiasonjob31.com>
2020-06-11 14:10:07 +0800

    Integer, numbers, floats,  BIDMAS rule and increment/decrement operator

---
+++ variables/numbers.php
@@ -0,0 +1,39 @@
+<?php 
+    $radius = 25;
+    $pi = 3.14;
+
+    // BASIC OPERATOR - *, /, +, -, **
+
+    // echo $pi * $radius**2;
+
+    // ORDER OF OPERATION (BIDMAS)
+    // echo 2 * (4 + 9) / 3;
+
+    // INCREMENT & DECREMENT OPERATORS
+    // echo ++$radius;
+    // echo --$radius;
+
+    // SHORTHAND OPERATORS
+    $age = 26;
+    $age += 10;
+    // echo $age;
+
+    $age *= 2;
+    // echo $age;
+
+    // NUMBER FUNCTIONS
+    // echo floor($pi); // result 3
+    // echo ceil($pi); // result 4
+    echo pi();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Numbers</title>
+</head>
+<body>
+    
+</body>
+</html>
